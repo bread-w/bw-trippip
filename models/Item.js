712413cd5d1e87f2b8af4662d16cb882a481918e
Brad Williams <jb.wms89@gmail.com>
2020-09-16 11:22:45 -0400

    added item, and index model, working on ItemController

---
+++ models/Item.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        require: "Item name is required.",
+    },
+    status: {
+        type: String,
+        enum: ["Inventory", "Wishlist"],
+        default: "Inventory"
+    },
+});
+
+const Item = mongoose.model("Item", ItemSchema);
+
+module.exports = Item;
